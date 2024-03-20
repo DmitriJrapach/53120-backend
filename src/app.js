be3a13f70c6b73a2b459d21a4e1f2867c8e75c98
Dmitri Jrapach <dmitrijrapach@gmail.com>
2024-03-20 18:28:11 -0300

    desafio numero 3

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
+
 import express from "express";
-import productRouter from "./routes/products.router.js";
-import cartRouter from './routes/cart.router.js'; 
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/cart.router.js";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
+import Sockets from "./sockets.js";
 
 const app = express();
+const port = 8080;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+//pequeño mod para que me lea las imagenes de public, despues ver si se puede mejorar
+app.use("/public", express.static("public"));
 
-// Usa el router de productos
-app.use("/api/products", productRouter);
-app.use('/api/carts', cartRouter);
+// Routes
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
-// Inicia el servidor
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
-});
+// Set up Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
+// Start server
+const server = app.listen(port, () =>
+  console.log(`Servidor corriendo en http://localhost:${port}`)
+);
+
+// Set up WebSocket server
+const io = new Server(server);
+Sockets(io);
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,21 @@
 
-import multer from "multer";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/img");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export const uploader = multer({storage});
+export default __dirname;
+
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "public/img");
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname);
+//     }
+// });
+
+// export const uploader = multer({storage});
